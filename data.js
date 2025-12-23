f2183c2621b73e8f73477cc0f956c6ef85f9d6bb
Unicorn-i0 <rtorrealba@miucsh.cl>
2025-12-23 20:27:07 -0300

    Update data.js

--- data.js
+++ data.js
@@ -6,15 +6,19 @@ const datosCursos = [
       {
         id: "S1",
         horario: [
-          { dia: "Lunes", inicio: "08:30", fin: "10:00", tipo: "Cátedra" },
-          { dia: "Miércoles", inicio: "16:00", fin: "17:30", tipo: "Ayudantía" }
+          // Clase dura 80 minutos: 08:30 a 09:50
+          { dia: "Lunes", inicio: "08:30", fin: "09:50", tipo: "Cátedra" }, 
+          // Ayudantía dura 80 minutos: 16:00 a 17:20
+          { dia: "Miércoles", inicio: "16:00", fin: "17:20", tipo: "Ayudantía" } 
         ]
       },
       {
         id: "S2",
         horario: [
-          { dia: "Martes", inicio: "10:15", fin: "11:45", tipo: "Cátedra" },
-          { dia: "Jueves", inicio: "14:30", fin: "16:00", tipo: "Ayudantía" }
+          // Clase dura 80 minutos: 10:15 a 11:35
+          { dia: "Martes", inicio: "10:15", fin: "11:35", tipo: "Cátedra" }, 
+          // Ayudantía dura 80 minutos: 14:30 a 15:50
+          { dia: "Jueves", inicio: "14:30", fin: "15:50", tipo: "Ayudantía" }
         ]
       }
     ]
@@ -26,15 +30,17 @@ const datosCursos = [
       {
         id: "S3",
         horario: [
-          { dia: "Lunes", inicio: "10:15", fin: "11:45", tipo: "Cátedra" },
-          { dia: "Miércoles", inicio: "10:15", fin: "11:45", tipo: "Cátedra" }
+          // Clases duran 80 minutos
+          { dia: "Lunes", inicio: "10:15", fin: "11:35", tipo: "Cátedra" }, 
+          { dia: "Miércoles", inicio: "10:15", fin: "11:35", tipo: "Cátedra" }
         ]
       },
       {
         id: "S4",
         horario: [
-          { dia: "Martes", inicio: "08:30", fin: "10:00", tipo: "Cátedra" },
-          { dia: "Jueves", inicio: "08:30", fin: "10:00", tipo: "Cátedra" }
+          // Clases duran 80 minutos
+          { dia: "Martes", inicio: "08:30", fin: "09:50", tipo: "Cátedra" }, 
+          { dia: "Jueves", inicio: "08:30", fin: "09:50", tipo: "Cátedra" }
         ]
       }
     ]
@@ -46,8 +52,10 @@ const datosCursos = [
       {
         id: "S1",
         horario: [
-          { dia: "Miércoles", inicio: "14:30", fin: "16:00", tipo: "Cátedra" },
-          { dia: "Viernes", inicio: "10:15", fin: "11:45", tipo: "Ayudantía" }
+          // Clase dura 80 minutos: 14:30 a 15:50
+          { dia: "Miércoles", inicio: "14:30", fin: "15:50", tipo: "Cátedra" }, 
+          // Ayudantía dura 80 minutos: 10:15 a 11:35
+          { dia: "Viernes", inicio: "10:15", fin: "11:35", tipo: "Ayudantía" }
         ]
       }
     ]
