6b5082b28d4bf156ae487eaf5b3d85d0775d6d1f
Unicorn-i0 <rtorrealba@miucsh.cl>
2025-12-23 20:18:58 -0300

    Create data.js
    Base de datos de los cursos a mostrar

---
+++ data.js
@@ -0,0 +1,55 @@
+const datosCursos = [
+  {
+    sigla: "INF101",
+    nombre: "Introducción a la Programación",
+    secciones: [
+      {
+        id: "S1",
+        horario: [
+          { dia: "Lunes", inicio: "08:30", fin: "10:00", tipo: "Cátedra" },
+          { dia: "Miércoles", inicio: "16:00", fin: "17:30", tipo: "Ayudantía" }
+        ]
+      },
+      {
+        id: "S2",
+        horario: [
+          { dia: "Martes", inicio: "10:15", fin: "11:45", tipo: "Cátedra" },
+          { dia: "Jueves", inicio: "14:30", fin: "16:00", tipo: "Ayudantía" }
+        ]
+      }
+    ]
+  },
+  {
+    sigla: "MAT202",
+    nombre: "Álgebra Avanzada",
+    secciones: [
+      {
+        id: "S3",
+        horario: [
+          { dia: "Lunes", inicio: "10:15", fin: "11:45", tipo: "Cátedra" },
+          { dia: "Miércoles", inicio: "10:15", fin: "11:45", tipo: "Cátedra" }
+        ]
+      },
+      {
+        id: "S4",
+        horario: [
+          { dia: "Martes", inicio: "08:30", fin: "10:00", tipo: "Cátedra" },
+          { dia: "Jueves", inicio: "08:30", fin: "10:00", tipo: "Cátedra" }
+        ]
+      }
+    ]
+  },
+  {
+    sigla: "INF303",
+    nombre: "Bases de Datos",
+    secciones: [
+      {
+        id: "S1",
+        horario: [
+          { dia: "Miércoles", inicio: "14:30", fin: "16:00", tipo: "Cátedra" },
+          { dia: "Viernes", inicio: "10:15", fin: "11:45", tipo: "Ayudantía" }
+        ]
+      }
+    ]
+  }
+];
